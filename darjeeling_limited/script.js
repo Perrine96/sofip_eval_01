f85430fbbd15838b5a5b6fb2d9efa24fc13da769
Perrine96 <perrinemallard96@gmail.com>
2025-05-06 16:12:05 +0200

    login + register

--- darjeeling_limited/script.js
+++ darjeeling_limited/script.js
@@ -1,1 +1,22 @@
-// LOGIN
+let username = document.querySelector('#username');
+let password = document.querySelector('#password');
+
+function store() {
+    localStorage.setItem('username', username.value);
+    localStorage.setItem('password', password.value);
+}
+
+localStorage.getItem('username');
+localStorage.getItem('password');
+
+// Vérifier si les données sont celles entrées dans le login
+// function check() {
+//     let storedUsername = localStorage.getItem('username');
+//     let storedPassword = localStorage.getItem('password');
+//     if (username.value == storedUsername && password.value == storedPassword) {
+//         alert('Connexion réussie !');
+//         window.location.href = 'index.html';
+//     } else {
+//         alert('Pseudo invalide, veuillez réessayer.');
+//     }
+// }

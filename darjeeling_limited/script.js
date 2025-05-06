f5f1b43ac339a3fefcf7e5a91275f0efc3fa40b9
Perrine96 <perrinemallard96@gmail.com>
2025-05-06 16:24:48 +0200

    login + register ok

--- darjeeling_limited/script.js
+++ darjeeling_limited/script.js
@@ -4,19 +4,32 @@ let password = document.querySelector('#password');
 function store() {
     localStorage.setItem('username', username.value);
     localStorage.setItem('password', password.value);
+    if (username.value == '' || password.value == '') {
+        alert('Veuillez remplir tous les champs.');
+        return false;
+    } else {
+        alert('Inscription réussie !');
+        window.location.href = 'login.html';
+    }
 }
 
-localStorage.getItem('username');
-localStorage.getItem('password');
+function check() {
+    let storedUsername = localStorage.getItem('username');
+    let storedPassword = localStorage.getItem('password');
+    if (username.value == storedUsername && password.value == storedPassword) {
+        alert('Connexion réussie !');
+        window.location.href = 'index.html';
+    } else {
+        alert('Pseudo ou mot de passe invalide, veuillez réessayer.');
+        document.getElementById("login-form").reset();
+    }
+}
 
-// Vérifier si les données sont celles entrées dans le login
-// function check() {
-//     let storedUsername = localStorage.getItem('username');
-//     let storedPassword = localStorage.getItem('password');
-//     if (username.value == storedUsername && password.value == storedPassword) {
-//         alert('Connexion réussie !');
-//         window.location.href = 'index.html';
-//     } else {
-//         alert('Pseudo invalide, veuillez réessayer.');
-//     }
-// }
+
+function returnToRegister() {
+    window.location.href = 'register.html';
+}
+
+function returnToLogin() {
+    window.location.href = 'login.html';
+}
